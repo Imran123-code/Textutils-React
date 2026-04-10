130327df9818084515a0347fa464270f3fc45548
Imran ahmad mir <letsmeetimran@gmail.com>
2026-04-10 22:24:58 +0530

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style={{display:'flex',alignContent:"center",justifyContent:'center',alignItems:"center"}}>
+      <p>All rights are reversed @2026</p>
+    </div>
+  )
+}
+
+export default Footer
